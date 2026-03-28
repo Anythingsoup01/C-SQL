fb70a8fbfe1ecb64b3f8a6ffe249cfba1253ca51
Anythingsoup01 <williamenglish1220@gmail.com>
2026-03-28 11:27:31 -0400

    initial commit

---
+++ prebuild.lua
@@ -0,0 +1,43 @@
+Workspace = {
+  name = "csql-wks",
+}
+
+Project = {
+  name = "csql",
+  kind = "StaticLib",
+  language = "C",
+  dialect = "23",
+
+  pch = "csqlpch.h",
+
+  files = {
+    "src/*.c",
+  },
+
+  includedirs = {
+    "include/",
+  },
+
+  links = {
+    "sqlite3",
+  },
+}
+
+Project = {
+  name = "test",
+  kind = "ConsoleApp",
+  language = "C",
+  dialect = "23",
+
+  files = {
+    "test/src/*.c",
+  },
+
+  includedirs = {
+    "include"
+  },
+
+  links = {
+    "csql"
+  }
+}
